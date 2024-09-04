a13cfdbcfcb646a93a3a86527e7e808498510657
gathei-njoroge <60644525+gathei-iraki@users.noreply.github.com>
2024-09-04 11:09:25 +0300

    added stars background to page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,10 @@ import { StarsBackground } from '../components/ui/stars-background';
 
 const HomePage = () => {
 return (
-    <div className="relative h-screen w-screen">
-        <StarsBackground className="z-0" />
-        <div className="relative z-10 flex items-center justify-center h-full">
-            <h1 className="text-white text-4xl">Welcome to Spotify Ten</h1>
-        
-        </div>
-    </div>
+  <div style={{ backgroundColor: 'black', height: '100vh', width: '100vw' }}>
+
+  <StarsBackground />
+</div>
 );
 };
 
